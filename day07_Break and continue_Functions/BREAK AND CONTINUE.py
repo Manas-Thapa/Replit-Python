49dc8388751348781d676b8125db37b51b2d8241
Manas-Thapa <mcfacts26@gmail.com>
2025-07-06 21:31:39 +0530

    Day 07

---
+++ day07_Break and continue_Functions/BREAK AND CONTINUE.py
@@ -0,0 +1,10 @@
+# BREAK
+for i in range (100):
+  print("5 X",i, "=",5*i)
+  if i == 10:
+    break
+# CONTINUE
+for i in range (1,100):
+  if i == 10:
+    continue
+  print("5 X",i, "=",5*i)
---
+++ day07_Break and continue_Functions/FUNCTIONS.py
@@ -0,0 +1,25 @@
+# FUNCTIONS
+def calculateGmean(a , b):
+   mean = (a*b)/(a+b)
+   print (mean)
+def isgreater (a, b):
+  if a>b:
+   print(a,"is greater than",b)
+  elif b>a:
+    print(b,"is greater than",a)
+  else:
+    print("Both are equal")
+    
+a=9
+b=10
+calculateGmean(a , b)
+c=2
+d=3
+calculateGmean(c , d)
+e=5
+f=7
+calculateGmean(e , f)
+isgreater(a,b)
+isgreater(b,d)
+def islesser(a, b):
+  pass
